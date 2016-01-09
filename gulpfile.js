c8d12d991db7b7712d3fd0c28dcd6b8911c793f2
WILLIAN RIBEIRO <WILLIAN@MacBook-Pro-de-Wiliam.local>
2016-01-09 19:09:37 -0200

    Add Gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,46 @@
 var gulp = require('gulp');
+var browserSync = require('browser-sync');
+var browserSyncReload = browserSync.reload;
+var compass = require('gulp-compass');
 
+/*
+    * ## TASK compass
+    * Compila os arquivos do SASS
+*/
+gulp.task('compass', function() {
+    gulp.src('./sass/**/*.scss')
+        .pipe(compass({
+            css: 'app/css',
+            sass: 'sass',
+            image: 'app/images'
+        }));
+});
+
+
+/*
+    * ## TASK watch
+    * Sobe um servidor para a aplicação e injeta recarrega o browser após
+    * alterações (scss, css, html, js)
+*/
+gulp.task('watch', function () {
+    browserSync({
+        'server' : { 'baseDir' : 'app'}
+    });
+
+    gulp.watch(
+        ['*.html', 'css/**/*.css', '**/*.js'],
+        { 'cwd' : 'app'},
+        browserSyncReload
+    );
+
+    gulp.watch('sass/**/*.scss', ['compass']);
+});
+
+
+/*
+    * ## TASK default
+    * Executa quando acionado apenas "gulp" no terminal
+*/
 gulp.task('default', function () {
 
 });
