a8fcffce9a438713cf43266cfdf6c6fe3eeef87a
Willian Ribeiro <willianribeiro.contato@gmail.com>
2016-06-03 15:55:37 -0300

    Change ong profile layout

--- app/modules/ong/controller/listCtrl.js
+++ app/modules/ong/controller/listCtrl.js
@@ -11,17 +11,38 @@
         init();
 
         function init() {
-            controller.isEditing = false;
+            // controller.isEditing = false;
             controller.currentTab = 'tasks';
+            controller.taskTags = [];
+            controller.taskStatus = getStatusOptions();
+            controller.taskStatusSelected = null;
+            controller.taskRecurrence = getRecurrenceOptions();
+            controller.taskRecurrenceSelected = null;
+            controller.taskDescription = null;
 
-            controller.toggleEdit = toggleEdit;
+            // controller.toggleEdit = toggleEdit;
             controller.setCurrentTab = setCurrentTab;
             controller.isCurrentTab = isCurrentTab;
         }
 
-        function toggleEdit() {
-            controller.isEditing = !controller.isEditing;
+        // TASK
+        function getRecurrenceOptions() {
+            var options = ['única', 'recorrente'];
+            options = options.map(function(option) {
+                return { label : option };
+            });
+            return options;
         }
+
+        // TASK
+        function getStatusOptions() {
+            var options = ['aberto', 'concluído', 'andamento'];
+            options = options.map(function(option) {
+                return { label : option };
+            });
+            return options;
+        }
+
 
         function setCurrentTab(tab) {
             controller.currentTab = tab;
